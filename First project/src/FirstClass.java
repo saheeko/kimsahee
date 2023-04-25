aa191a3f9b369410151c0fa1828d978fa74c7375
user <user@192.168.0.18>
2023-04-25 17:25:03 +0900

    aq

---
+++ First project/src/FirstClass.java
@@ -0,0 +1,15 @@
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+//		System.out.println("이 문장이 실행될까요?...");
+		
+		System.out.println("이 문장이 실행될까요?...");
+/*		
+		System.out.println("이 문장이 실행될까요?...");
+		System.out.println("이 문장이 실행될까요?...");
+*/		
+	}
+
+}
---
+++ First project/src/q501/Main.java
@@ -0,0 +1,8 @@
+package q501;
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Fun Programming!");
+	}
+}
