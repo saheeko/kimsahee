761f0f0060340314bba530baf887e30a47b0d2ac
user <user@192.168.0.18>
2023-04-26 15:34:07 +0900

    test

---
+++ First project/src/q509/Main.java
@@ -0,0 +1,13 @@
+package q509;
+
+public class Main {
+	public static void main(String[]args) {
+			int x = -100;
+			
+			System.out.println(x);
+			
+			
+	
+
+	}
+}
---
+++ First project/src/q9003/Main.java
@@ -0,0 +1,13 @@
+package q9003;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("세 번째 프로그램입니다.");
+		System.out.println("줄을 바꾸어 출력합니다.");
+		System.out.println("여기까지 출력하고");
+		System.out.println("줄을 바꿉니다.");
+	}
+
+}
